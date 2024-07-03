8626fdb55f08db82ebac3477dfdd5b8f9be560da
raghad <raghadmandili@gmail.com>
2024-07-04 01:48:05 +0300

    buttons using grid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,7 +3,28 @@
         الة حاسبة
     </title>
 </svelte:head>
-<div class="bg-white h-[30rem] w-[20rem] rounded-3xl flex justify-center items-center h[100vh]">
+<div class="bg-white h-fit w-fit rounded-3xl grid grid-cols-4 gap-1">
+<button class="bg-[#f3c0d1cb]">%</button>
+<button class="bg-[#f3c0d1cb]">%</button>
+<button class="bg-[#f3c0d1cb]">CE</button>
+<button class="bg-[#f81b98]">C</button>
+<button>7</button>
+<button>8</button>
+<button>9</button>
+<button class="bg-[#f81b98]">-</button>
+<button>4</button>
+<button>5</button>
+<button>6</button>
+<button class="bg-[#f81b98]"></button>
+<button>1</button>
+<button>2</button>
+<button>3</button>
+<button class="bg-[#f81b98]">x</button>
+<button>.</button>
+<button>0</button>
+<button>=</button>
+<button class="bg-[#f81b98]">+</button>
+
 
 </div>
 
